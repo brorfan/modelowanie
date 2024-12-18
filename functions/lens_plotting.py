7320a9c6512df37911d68385791bdbcbdb18668b
anna <s198824@student.pg.edu.pl>
2024-12-18 11:51:19 +0100

    fixed lens

--- functions/lens_plotting.py
+++ functions/lens_plotting.py
@@ -12,7 +12,7 @@ ax.axis('off')  # an option to turn off the axis
 
 # right lens
 centerA = (0, 0)  # center coordinates
-radiusA = 2  # radius of the circle (right side of the lens)
+radiusA = -2  # radius of the circle (right side of the lens)
 angleA = 180  # angle range in degrees (from 0 to 360)
 thetaA = 90  # starting angle for the arc
 widthA = radiusA # width of the arc
@@ -27,9 +27,16 @@ widthB = radiusB
 heightB = 10
 
 # rectangle in the middle
-widthR = max(widthA, widthB)
+widthR = max(abs(widthA), abs(widthB))
 heightR = heightA
 centerR = (centerA[0] - (widthR / 2), centerA[1] - (heightR / 2))
+
+# lines instead of the rectangle
+lineA_height = centerA[1] + (heightA / 2)
+lineB_height = centerA[1] - (heightA / 2)
+lines_start = centerA[0] - (max(abs(widthA), abs(widthB)) / 2)
+lines_length = centerA[0] + (max(abs(widthA), abs(widthB)) / 2)
+
 
 fixed_centerA = (centerA[0] + (widthR/2), centerA[1])
 fixed_centerB = (centerB[0] - (widthR/2), centerB[1])
@@ -43,32 +50,45 @@ arcB = patches.Arc(fixed_centerB, widthB, heightB, angle=angleB, theta1=thetaB, 
 rectangle = patches.Rectangle(centerR, widthR, heightR,
                               edgecolor='black', linewidth=2, facecolor='white')
 
+lines = plt.hlines((lineA_height, lineB_height), lines_start, lines_length, colors='black', linestyles='solid', linewidth=2)
+
+# line_left = plt.vlines(lines_start, lineA_height, lineB_height, colors='black', linestyles='solid', linewidth=2)
+
+# line_right = plt.vlines(lines_length, lineA_height, lineB_height, colors='black', linestyles='solid', linewidth=2)
+
 arcA.angle = 180
 arcB.angle = 180
+
 if radiusA > 0:
     ax.add_patch(arcA)
-    ax.add_patch(rectangle)
     if radiusB > 0:
         ax.add_patch(arcB)
     elif radiusB < 0:
         arcB.angle = 360
         ax.add_patch(arcB)
+    elif radiusB == 0:
+        line_left = plt.vlines(lines_start, lineA_height, lineB_height, colors='black', linestyles='solid', linewidth=2)
+
 elif radiusA < 0:
     arcA.angle = 360
-    ax.add_patch(rectangle)
     ax.add_patch(arcA)
     if radiusB > 0:
         ax.add_patch(arcB)
     elif radiusB < 0:
         arcB.angle = 360
         ax.add_patch(arcB)
+    elif radiusB == 0:
+        line_left = plt.vlines(lines_start, lineA_height, lineB_height, colors='black', linestyles='solid', linewidth=2)
+
 elif radiusA == 0:
-    ax.add_patch(rectangle)
+    line_right = plt.vlines(lines_length, lineA_height, lineB_height, colors='black', linestyles='solid')
     if radiusB > 0:
         ax.add_patch(arcB)
     elif radiusB < 0:
         arcB.angle = 360
         ax.add_patch(arcB)
+    elif radiusB == 0:
+        line_left = plt.vlines(lines_start, lineA_height, lineB_height, colors='black', linestyles='solid', linewidth=2)
 
 
 plt.show()
